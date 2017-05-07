06defab4acb415f1fb8dbcb2c0e5333f30e764b3
taka-sho <takasho530000@gmail.com>
2017-05-07 18:42:08 +0900

    chore(index.test) add option

--- specs/index.test.js
+++ specs/index.test.js
@@ -1,21 +1,17 @@
 const assert = require('power-assert')
 const browser = require('protractor').browser
-const chai = require('chai');
-const chaiAsPromised = require('chai-as-promised');
 
 describe('protractor test', () => {
-    chai.use(chaiAsPromised);
-    const expect = chai.expect;
+
+    beforeEach(function() {
+      browser.ignoreSynchronization = true;
+    });
+
     it('google.co.jp', (done) => {
-        // browser.get('https://google.co.jp');
-        // assert.equal(browser.getTitle(), 'Google')
-        // expect(browser.getTitle()).toEqual('Google');
-        // browser.getTitle().then((title) => {
-        //     console.log(title)
-        //     done()
-        // })
-    browser.get('http://juliemr.github.io/protractor-demo/');
-    expect(browser.getTitle()).toEqual('Super Calculator');
-    done();
+    browser.get('http://uncovertruth.github.io/examples/');
+    browser.getTitle().then((title) => {
+      assert.equal(title, 'USERDIVE examples')
+      done();
+    })
     })
 })
