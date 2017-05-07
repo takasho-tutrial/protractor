b3a47cae1a4edcd30f287e8ceeffbf99e1a94cf2
taka-sho <takasho530000@gmail.com>
2017-05-07 21:16:44 +0900

    chore(protfactor.config) use es2015

--- specs/index.test.js
+++ specs/index.test.js
@@ -1,17 +1,18 @@
-const assert = require('power-assert')
-const browser = require('protractor').browser
+import assert from 'power-assert';
+import { browser } from 'protractor';
+import { describe, beforeEach, it } from 'mocha';
 
 describe('protractor test', () => {
 
-    beforeEach(function() {
-      browser.ignoreSynchronization = true;
-    });
+  beforeEach(function() {
+    browser.ignoreSynchronization = true;
+  });
 
-    it('UT Examples', (done) => {
+  it('UT Examples', (done) => {
     browser.get('http://uncovertruth.github.io/examples/');
     browser.getTitle().then((title) => {
-      assert.equal(title, 'USERDIVE examples')
+      assert.equal(title, 'USERDIVE examples');
       done();
     })
-    })
+  });
 })
